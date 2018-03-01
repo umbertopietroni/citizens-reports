dd6c9f9e3879e84f31302dd920fdb010649aec70
umbertopietroni <32171743+umbertopietroni@users.noreply.github.com>
2018-03-01 09:29:15 +0100

    Delete downloadnegrar.py

--- downloadnegrar.py
+++
@@ -1,8 +0,0 @@
-import urllib
-import string
-import time
-for i in range(4908):
-	print("%s ..." % i)
-	txt = urllib.urlopen("http://www.comunenegrar.it/c023052/gu/gu_p_consulta.php?idguasto=%s&x=" % i).read()
-	open('sources/negrar/' + string.zfill(i, 4) + '.html', 'w').write(txt)
-	time.sleep(0.2)
