69b6b6afc17564364db8b55e0c9858162bb4167b
Giulio Angiani <giulio.angiani@gmail.com>
2018-03-23 13:17:52 +0100

    MAIN COMMIT: refactoring struttura progetto

--- dbinterface/classes.py
+++ dbinterface/classes.py
@@ -1,9 +1,26 @@
+from db_interface import *
+from functools import wraps
+import pprint
+
+def add_properties(fnz):
+	@wraps(fnz)
+	def f(pard, *args, **kwargs):
+		classname = f.__name__
+		for k in f.__dict__["_{}__info".format(classname)]:
+#			print(k)
+			prop = (lambda self: print(k))
+#			print(prop, dir(prop))
+			setattr (f, k, property(prop))
+		return fnz(pard, *args, **kwargs)
+	return f
+
 class IssueImage:
 	
 	__file = None
 	__content = None
 	__classification_dict = {}
 	__category = None
+	__confidence = None
 	
 	def __init__(self, filename):
 		self.__file = filename
@@ -19,21 +36,33 @@ class IssueImage:
 	def getCategory(self):
 		return self.__category
 		
-	def setCategory(self, category):
-		self.__category = category
+	def getConfidence(self):
+		return self.__confidence
+		
+#	def setCategory(self, category):
+#		self.__category = category
 		
 	def getClassificationDict(self):
 		return self.__classification_dict
 
 	def setClassificationDict(self, classification_dict):
 		self.__classification_dict = classification_dict
+		self.__category = list(classification_dict.keys())[list(classification_dict.values()).index(max(classification_dict.values()))]
+		self.__confidence = max(classification_dict.values())
 		
+
+#@add_properties	
 class Issue:
 	
 	__info = {
 		"id": None,
-		"phone_number": None,
-		"phone_user": None,
+		"phone_number": '',
+		"user_id": None,
+		"msg_id": None,
+		"channel": '',
+		"username": '',
+		"firstname":'',
+		"lastname":'',
 		"date": None,
 		"time": None,
 		"text": "",
@@ -48,21 +77,27 @@ class Issue:
 	__status = None
 	__classification_dict = {}
 	
-	def __init__(self):
-		pass
+	def __init__(self, msg_id, username):
+		self.__info["id"]=msg_id
+		self.__info["phone_user"]=username
 		
 	def save(self):
-		# save data to DB
-		pass
+		saveIssue(self)
 		
-	def getInfo(self):
-		return self.__info
+	def getInfo(self, param=''):
+		if param:
+			if param in self.__info.keys():
+				return self.__info[param]
+			else:
+				raise NameError("parameter '{}' is not valid".format(param))
+		else:
+			return self.__info
 		
 	def setInfo(self, param, value):
 		if param in self.__info.keys():
 			self.__info[param] = value
 		else:
-			raise NameError("parameter 'param' is not valid")
+			raise NameError("parameter '{}' is not valid".format(param))
 			
 	def addImage(self, i: IssueImage):
 		if not isinstance(i, IssueImage):
@@ -72,6 +107,11 @@ class Issue:
 			
 	def getImages(self):
 		return 	self.__info["images"]
+		
+	def getUserId(self):	
+		return 	self.__info["user_id"]
+	def getMsgId(self):	
+		return 	self.__info["msg_id"]
 		
 	def getStatus(self):
 		return self.__status
@@ -90,7 +130,10 @@ class Issue:
 
 	def setClassificationDict(self, classification_dict):
 		self.__classification_dict = classification_dict
-		
+	
+	def printIssue(self):
+		print(self.__info, self.__category)
+
 	def getLatitude(self):
 		return self.__info["position"]["latitude"]
 		
@@ -102,4 +145,7 @@ class Issue:
 		
 	def setLongitude(self, lon):
 		self.__info["position"]["longitude"] = lon
-
+		
+	@staticmethod
+	def getIssueByMsgIdUserId(user_id, msg_id):
+		return None
---
+++ other/telegram_bot/db_interface.py
@@ -0,0 +1,113 @@
+import MySQLdb
+import MySQLdb.cursors
+from connection_parameters import *
+
+def makesql(sql):
+	db=MySQLdb.connect(host=DBHOST, 
+					user=DBUSER, 
+					passwd=DBPASW, 
+					db=DBNAME, 
+					cursorclass=MySQLdb.cursors.DictCursor)
+
+	c=db.cursor()
+	c.execute(sql)
+	rows = c.fetchall()
+	db.commit()
+	return rows
+
+def getIssueIdByUserIdMsgId(user_id, msg_id):
+	sql = """select id 
+			 from issues
+			 where user_id = '{}'
+			 and msg_id = '{}'
+			""".format(user_id, msg_id)
+	#print(sql)
+	t = makesql(sql)
+	#print(t)
+	if not t:
+		return ""
+	return t[0]["id"]
+
+def getUserIdByChannelMsgId(user_id, channel):
+	sql = """select id 
+			 from users
+			 where user_id = '{}'
+			 and channel = '{}'
+			""".format(user_id, channel)
+	#print(sql)
+	t = makesql(sql)
+	#print(t)
+	if not t:
+		return ""
+	return t[0]["id"]
+
+def getLastInsertedId(table):
+	t = makesql("select max(id) as last_insert_id from {}".format(table))
+	if not t:
+		return None
+	else:
+		return t[0]["last_insert_id"]
+
+def saveIssue(issue):
+#	print("SAVE", issue.getInfo(), issue.getImages(), issue.getClassificationDict())
+	db_issue_id = getIssueIdByUserIdMsgId(issue.getUserId(), issue.getMsgId())
+#	print("db_issue_id=", db_issue_id)
+	
+	# SAVE NEW
+	info = issue.getInfo()
+	info.update(issue.getInfo()["position"])
+	info['category'] = issue.getCategory()
+	info['status'] = "NEW"
+	info['classification_dict'] = issue.getClassificationDict()
+	sql = """replace into issues(id, day, time, 
+								user_id, msg_id,
+								latitude, longitude,
+								channel, text,
+								category, status,
+								classification_dict)
+				values("%%(db_issue_id)s", "%(date)s", "%(time)s",
+						"%(user_id)s", "%(msg_id)s", 
+						"%(latitude)s", "%(longitude)s", 
+						"%(channel)s", "%(text)s", 
+						"%(category)s", "%(status)s", 
+						"%(classification_dict)s"
+						)
+			""" % info % vars()
+#	print(sql)
+	makesql(sql)
+	
+	if not db_issue_id:
+		# recupera ultimo id 
+		db_issue_id = getLastInsertedId("issues")
+#		print("ID ISSUE : ", db_issue_id)
+	else:
+#		print("EXISTING ISSUE")
+		pass
+	# SAVE ALL ISSUE IMAGES 
+	sql = "delete from images where issue_id = '{}'".format(db_issue_id)
+	makesql(sql)
+	for i in issue.getImages():
+#		print(i.getFilename())
+#		print(i.getClassificationDict())
+#		print(i.getCategory())
+#		print(i.getConfidence())
+		
+		sql = """insert into images(issue_id, filename, category, classification_dict)
+					values("{}", "{}", "{}", "{}")""".format(db_issue_id, 
+															i.getFilename(),
+															i.getCategory(),
+															i.getClassificationDict())
+		makesql(sql)
+
+	# save user info
+	
+	db_user_id = getUserIdByChannelMsgId(issue.getUserId(), issue.getInfo("channel"))
+	
+	sql = """replace into users(id, user_id, channel, phone_number, description) 
+			values("{}", "{}", "{}", "{}", "{}")""".format(db_user_id,
+															issue.getUserId(), 
+															issue.getInfo("channel"),
+															issue.getInfo("phone_number"),
+															issue.getInfo("firstname") + " " + issue.getInfo("lastname"))
+#	print(sql)
+	makesql(sql)
