1553e7e54f2d4f7654f01fa6c71812b058dab69f
umbertopietroni <u.pietroni@gmail.com>
2018-07-18 19:04:48 +0200

    UPD: whatsapp bot

--- whatsapp/main.py
+++ whatsapp/main.py
@@ -13,7 +13,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import re
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 import pickle
 
 import sys
@@ -39,27 +39,38 @@ def delete_chat(driver, chat_id):
     actionChains = ActionChains(driver)
     contacts = usersDiv.find_elements_by_class_name("_2wP_Y")
     print("inizio delete")
-    
-    
+
     for contact in contacts:
         chatTitle = contact.find_elements_by_class_name("_1wjpf")
         if chatTitle[0].text == chat_id:
-            contact.click()
-            # actionChains.move_to_element(contact)
-            # actionChains.perform()
-            contact.find_element_by_class_name("ZR5SB").click()
+            try:
+                contact.click()
 
-            time.sleep(1)
-            buttons_menu = driver.find_element_by_class_name("_3s1D4")
-            buttons_menu.find_elements_by_class_name("_3lSL5")[2].click()
-            # usersDiv.find_element_by_xpath('//*[@title="Elimina chat"]').click()
-            # driver.implicitly_wait(2)
-            wait = WebDriverWait(driver, 10)
-            #pop_up = wait.until(EC.presence_of_element_located((By.CLASS_NAME, '_1CnF3')))
-            time.sleep(1)
-            pop_up = driver.find_element_by_class_name("_1CnF3")
-            if pop_up:
-                pop_up.find_elements_by_class_name("_1WZqU")[1].click()
+                wait = WebDriverWait(driver, 10)
+                apri_menu = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'ZR5SB')))
+                apri_menu.click()
+                """apertura menu a tendina"""
+                time.sleep(1)
+                buttons_menu = wait.until(EC.presence_of_element_located((By.CLASS_NAME, '_3s1D4')))
+                #buttons_menu = driver.find_element_by_class_name("_3s1D4")
+
+                wait2 = WebDriverWait(buttons_menu,10)
+                wait2.until(EC.presence_of_element_located((By.CLASS_NAME, '_3lSL5')))
+                buttons_menu.find_elements_by_class_name("_3lSL5")[2].click()
+                """clicco sul bottone per eliminare chat"""
+                time.sleep(1)
+                pop_up = wait.until(EC.presence_of_element_located((By.CLASS_NAME, '_1CnF3')))
+                # pop_up = driver.find_element_by_class_name("_1CnF3")
+                if pop_up:
+                    wait3 = WebDriverWait(pop_up, 10)
+                    wait3.until(EC.element_to_be_clickable((By.CLASS_NAME, '_1WZqU')))
+                    pop_up.find_elements_by_class_name("_1WZqU")[1].click()
+                    time.sleep(1)
+                break
+            except Exception:
+                print("Errore nella delete")
+                print(traceback.format_exc())
+                driver.get('https://web.whatsapp.com')
 
 
 
@@ -70,7 +81,6 @@ def get_date_from_msg(text):
     date = datetime(y, mo, d, h, mi)
     print(date)
     return date
-
 
 
 """
@@ -106,14 +116,14 @@ def save_msg(driver, messageList, chatTitle):
                 # is_pop = im.find_elements_by_class_name("_1Qnxi")
                 if pos_class == "_1Qnxi":
                     print("posizione trovata")
-                    #pos = father.get_attribute("href")
+                    # pos = father.get_attribute("href")
                     pos = im.get_attribute("src")
                     if ("maps" in pos):
-                            
+
                         # https://maps.google.com/maps?q=44.7649225%2C10.3085252&z=17&hl=it
                         # print(pos)
-                        #lat_rex = re.compile(r'q=(.+?)%2C')
-                        #long_rex = re.compile(r'%2C(.+?)&z=')
+                        # lat_rex = re.compile(r'q=(.+?)%2C')
+                        # long_rex = re.compile(r'%2C(.+?)&z=')
                         lat_rex = re.compile(r'%7C(.+?)%2C%20')
                         long_rex = re.compile(r'%2C%20(.+?)&signature')
                         if lat_rex.findall(pos) and long_rex.findall(pos):
@@ -122,17 +132,18 @@ def save_msg(driver, messageList, chatTitle):
                             location = [lat, lon]
                             print(location)
 
-
+                elif pos_class == "_2H6ea":
+                    print("link esterno")
 
                 else:
                     src = im.get_attribute("src").replace("blob:", "")
 
-                    #testo aggiunto come didascalia alla foto
+                    # testo aggiunto come didascalia alla foto
                     didasc = m.find_elements_by_class_name("_1RiwZ")
                     if didasc:
                         did_text = didasc[0].find_elements_by_class_name("copyable-text")
                         if did_text:
-                            text += did_text[0].text+' '
+                            text += did_text[0].text + ' '
 
                     im.click()
                     # mpt = driver.find_elements_by_class_name("media-panel-tools")[0]
@@ -152,11 +163,11 @@ def save_msg(driver, messageList, chatTitle):
                     if firstfile.startswith("WhatsApp"):
                         print("Moving file : " + DOWNLOAD_PATH + firstfile)
                         downloaded_fname = firstfile.replace("WhatsApp Image ", str(datetime.now()))
-                        #downloaded_fname = downloaded_fname.replace(" ", "_")
-                        #downloaded_fname = downloaded_fname.replace(" ", "_")
-                        #downloaded_fname = downloaded_fname.replace("_at_", "__")
-                        
-                        #downloaded_fname = datetime.now()
+                        # downloaded_fname = downloaded_fname.replace(" ", "_")
+                        # downloaded_fname = downloaded_fname.replace(" ", "_")
+                        # downloaded_fname = downloaded_fname.replace("_at_", "__")
+
+                        # downloaded_fname = datetime.now()
 
                         shutil.move(DOWNLOAD_PATH + firstfile, msg_dir + "/%s" % downloaded_fname)
                         photo.append(msg_dir + "/%s" % downloaded_fname)
@@ -164,13 +175,18 @@ def save_msg(driver, messageList, chatTitle):
                     # content = open(filename, 'rb').read()
 
             elif copyableText:
-                #for ct in copyableText[0:1]:
-                #    print("ct = ", ct.text)
-                ct = copyableText[1]
+                # for ct in copyableText[0:1]:
+                #  print("ct = ", ct.text)
+                try:
+                    ct = copyableText[1]
+                except IndexError:
+                    for ct in copyableText[0:1]:
+                        print("ct = ", ct.text)
 
-                # precise timestamp data-pre-plain-text = [16:52, 18/3/2018] Guido Dondi
+                # precise timestamp data-pre-plain-text = [16:52, 18/3/2018]
                 pre_text = copyableText[0].get_attribute("data-pre-plain-text")
-                date = get_date_from_msg(pre_text)
+                if pre_text:
+                    date = get_date_from_msg(pre_text)
                 # timestamp = m.find_elements_by_class_name("_2f-RV")
                 # for ts in timestamp[0:1]:
                 # print("ts = ", ts.text)
@@ -178,7 +194,7 @@ def save_msg(driver, messageList, chatTitle):
                 text += ct.text + ' '
     if (not date):
         date = datetime.now()
-        
+
     m = {
         "message_id": ts,
         "date": date,
@@ -200,7 +216,7 @@ def save_msg(driver, messageList, chatTitle):
 
 def is_timeout(date):
     now = datetime.now()
-    delta = timedelta(seconds=60)
+    delta = timedelta(seconds=180)
     print(now - date)
 
     if now - date > delta:
@@ -228,8 +244,8 @@ def msg_to_segnalazione(msg, msg_dir):
         segnalazione.setInfo("text", text)
         if date:
             segnalazione.setInfo("datetime", date.strftime("%Y-%m-%d %H:%M:%S"))
-            #segnalazione.setInfo("date", date.strftime("%Y-%m-%d"))
-            #segnalazione.setInfo("time", date.strftime("%H:%M:%S"))
+            # segnalazione.setInfo("date", date.strftime("%Y-%m-%d"))
+            # segnalazione.setInfo("time", date.strftime("%H:%M:%S"))
         segnalazione.setInfo("user_id", user_id)
         segnalazione.setInfo("phone_number", user_id)
 
@@ -249,8 +265,8 @@ def msg_to_segnalazione(msg, msg_dir):
 
 
 def main(driver, chatHistory, replyQueue, firstRun):
-    #driver.switch_to_window(driver.window_handles[0])
-    
+    # driver.switch_to_window(driver.window_handles[0])
+
     try:
         usersDiv = driver.find_element_by_id("side")
         actionChains = ActionChains(driver)
@@ -259,7 +275,6 @@ def main(driver, chatHistory, replyQueue, firstRun):
         contacts = []
         print("Caricamento...")
     # messageDiv = driver.find_element_by_id("main")
-    
 
     new_messages = []
     received_msgs = []
@@ -268,14 +283,11 @@ def main(driver, chatHistory, replyQueue, firstRun):
     # <span data-icon="default-user"
     # <span data-icon="default-group"
 
-
-    
     for contact in contacts:
         chatTitle = contact.find_elements_by_class_name("_1wjpf")
         # print("chatTitle = ", chatTitle[0].text)
-        #unread = contact.find_elements_by_class_name("unread")
+        # unread = contact.find_elements_by_class_name("unread")
         # forse al posto di unread ci vuole "OUeyt"
-
 
         data_icon = contact.find_element_by_class_name("_3ZW2E")
         try:
@@ -284,16 +296,15 @@ def main(driver, chatHistory, replyQueue, firstRun):
             user = None
             print ("Gruppo")
 
-
         if user:
             print(chatTitle[0].text)
             # click on contact element and read all incoming messages
             ct = ""
             ts = ""
             messageList = []
-            driver.implicitly_wait(1.5)
-
-            
+            messageDiv =[]
+            messageOut=[]
+            #driver.implicitly_wait(1.5)
 
             print("=" * 100)
             try:
@@ -304,16 +315,18 @@ def main(driver, chatHistory, replyQueue, firstRun):
                 print("No contact")
             if not messageOut:
                 ##INVIO MESSAGGIO INIZIALE
-                    if not phoneExists(chatTitle[0].text):
-                        input_box = driver.find_element_by_class_name('_2S1VP')
-                        input_box.send_keys("Benvenuto! Può inviare altri messaggi, foto o posizione. La segnalazione verrà presa in carico dal sistema qualche minuto dopo l'ultimo messaggio. Per salvare la segnalazione il sistema conserverà anche il suo numero di telefono. Se non vuole che ciò accada invii un messaggio con scritto: \"Cancellami\" e la segnalazione non verrà salvata. Può anche eliminare il suo numero in qualsiasi momento inviando lo stesso messaggio \"Cancellami\".")
-                        driver.find_element_by_xpath('//span[@data-icon="send"]').click()
+                if not phoneExists(chatTitle[0].text):
+                    input_box = driver.find_element_by_class_name('_2S1VP')
+                    input_box.send_keys(
+                        "Benvenuto! Può inviare altri messaggi, foto o posizione. La segnalazione verrà presa in carico dal sistema qualche minuto dopo l'ultimo messaggio. Per salvare la segnalazione il sistema conserverà anche il suo numero di telefono. Se non vuole che ciò accada invii un messaggio con scritto: \"Cancellami\" e la segnalazione non verrà salvata. Può anche eliminare il suo numero in qualsiasi momento inviando lo stesso messaggio \"Cancellami\".")
+                    driver.find_element_by_xpath('//span[@data-icon="send"]').click()
 
-                    else:
-                        input_box = driver.find_element_by_class_name('_2S1VP')
-                        input_box.send_keys("Grazie! Può inviare altri messaggi, foto o posizione. La segnalazione verrà presa in carico dal sistema qualche minuto dopo l'ultimo messaggio e le sarà inviato un messaggio di conferma.")
-                        driver.find_element_by_xpath('//span[@data-icon="send"]').click()
-                    #time.sleep(0.5)
+                else:
+                    input_box = driver.find_element_by_class_name('_2S1VP')
+                    input_box.send_keys(
+                        "Grazie! Può inviare altri messaggi, foto o posizione. La segnalazione verrà presa in carico dal sistema qualche minuto dopo l'ultimo messaggio e le sarà inviato un messaggio di conferma.")
+                    driver.find_element_by_xpath('//span[@data-icon="send"]').click()
+                # time.sleep(0.5)
             try:
                 messageDiv = driver.find_element_by_id("main")
                 messageList = messageDiv.find_elements_by_class_name("message-in")
@@ -325,23 +338,22 @@ def main(driver, chatHistory, replyQueue, firstRun):
             if messageList:
                 ##cerco ultimo messaggio, testo o foto e calcolo tempo passato
                 last_msg = messageList[-1]
+                date = ""
                 copyableText = last_msg.find_elements_by_class_name("copyable-text")
                 if copyableText:
-                    # precise timestamp -> data-pre-plain-text = [16:52, 18/3/2018] Guido Dondi:
-                    text = copyableText[0].text.lower().replace(" ", "").replace("!", "").replace("'", "").replace(".", "").replace(",", "")
-                    if (chatTitle[0].text == "+39 348 123 2452" and text=="restart"):
-                        delete_chat(driver, chatTitle[0].text)
-                        driver.get('https://web.whatsapp.com')
+                    # precise timestamp -> data-pre-plain-text = [16:52, 18/3/2018]
+                    text = copyableText[0].text.lower().replace(" ", "").replace("!", "").replace("'", "").replace(".","").replace(",", "")
                     if text == "cancellami":
                         phoneDelete(chatTitle[0].text)
                         input_box = driver.find_element_by_class_name('_2S1VP')
                         input_box.send_keys("Elimino la segnalazione e il numero. Grazie e arrivederci.")
                         driver.find_element_by_xpath('//span[@data-icon="send"]').click()
                         delete_chat(driver, chatTitle[0].text)
-                        continue
+                        break
                     pre_text = copyableText[0].get_attribute("data-pre-plain-text")
-                    date = get_date_from_msg(pre_text)
-                else:
+                    if pre_text:
+                        date = get_date_from_msg(pre_text)
+                if not date:
                     time_d = last_msg.find_element_by_class_name("_3EFt_")
                     print(time_d.text)
                     h, mi = [int(s) for s in re.findall(r'\d+', time_d.text)]
@@ -349,20 +361,16 @@ def main(driver, chatHistory, replyQueue, firstRun):
                     date = now.replace(hour=h, minute=mi)
 
                 if is_timeout(date):
-                    pass
                     save_msg(driver, messageList, chatTitle[0].text)
 
                     ##INVIO MESSAGGIO
                     input_box = driver.find_element_by_class_name('_2S1VP')
                     input_box.send_keys("Segnalazione Ricevuta, Grazie!")
-                    driver.find_element_by_xpath('//span[@data-icon="send"]').click()
+
+                    wait = WebDriverWait(driver, 10)
+                    send = wait.until(EC.element_to_be_clickable((By.XPATH, '//span[@data-icon="send"]')))
+                    # driver.find_element_by_xpath('//span[@data-icon="send"]').click()
+                    send.click()
 
                     delete_chat(driver, chatTitle[0].text)
-                    
-                    
-                    
-                    
-                    
-
-
-
+                    break
